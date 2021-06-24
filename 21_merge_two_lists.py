31e3b9fda86aa3c849ad968508ee225f4bf441fa
bryantruong <bryantruong3139@yahoo.com>
2021-06-24 13:49:18 -0400

    Horrible attempt at merging two lists.

---
+++ 21_merge_two_lists.py
@@ -0,0 +1,129 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def createLinkedList(node_list, cycle_index_pos):
+    """
+    :param node_list: List of nodes to be put into list
+    :param cycle_index_pos: The index that the last node in node_list should point to, creating a cycle.
+    :return:
+    """
+
+    def search_by_index(head, index):
+        current_node = head
+        for i in range(index):
+            current_node = current_node.next
+        return current_node
+
+    # Function to insert node
+    def insert(root, item, items_next=None):
+        # This will only get used if this is going to be the head
+        new_node = ListNode(item)
+        if items_next:
+            node_to_link_to = search_by_index(root, items_next)
+            new_node.next = node_to_link_to
+        if not root:
+            root = new_node
+        else:
+            # We have to iterate through to the end of the list
+            current = root
+            while current.next:
+                current = current.next
+            current.next = new_node
+        return root
+
+    root = None
+    for i in range(len(node_list)):
+        if i == (len(node_list) - 1):
+            root = insert(root, node_list[i], cycle_index_pos)
+        else:
+            root = insert(root, node_list[i])
+    return root
+
+
+def printLinkedList(head):
+    """
+    :param head: Node that is the head of list.
+    :return: void, only prints
+    """
+    output_string = ""
+    while True:
+        output_string += str(head.val)
+        if not head.next:
+            break
+        else:
+            output_string += "->"
+            head = head.next
+    print(output_string)
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ############################################################
+        # My solution actually sucks. Somehow I way overthought it...
+        # Just google to find an actually good solution. Mine has same time complexity though.
+        ############################################################
+        # Rare Cases
+        if not l1 and not l2:
+            return None
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+
+        # First determine which list will be the node that we start with (and add other list into)
+        if l1.val <= l2.val:
+            main = l1
+            to_add = l2
+        else:
+            main = l2
+            to_add = l1
+        # Save off the head of the main list to return
+        main_head = main
+        # Save off the next node in main, since we'll need it to reconnect the incoming merged section
+        main_next = main.next
+
+        # Begin iterating through the lists. Can stop once the second list (to_add list) is merged fully merged in
+        while to_add:
+            # When the main list has been iterated through, and we just need to append to_add to it
+            if main_next is None:
+                main.next = to_add
+                # Break out of the while loop
+                to_add = None
+            # Check if the current value does not fit between the interval. If not, move the main pointers.
+            elif to_add.val > main_next.val:  # We already know that it is greater than or equal to main_next
+                # Move on to the next pointers for main
+                main = main_next
+                main_next = main_next.next
+            else:
+                # Need to save the current node, as well as the starting node of the to_add chain
+                curr_to_add = to_add
+                while curr_to_add.next:
+                    # At this point, if we are in this "else" block, we already know that to_add's value is valid.
+                    if curr_to_add.next.val <= main_next.val:
+                        # Move to the next node
+                        curr_to_add = curr_to_add.next
+                    else:
+                        break
+                # Rewire
+                main.next = to_add
+                to_add = curr_to_add.next
+                curr_to_add.next = main_next
+
+                # Advance the pointer for the next iteration of the outer loop
+                main = main_next
+                main_next = main_next.next
+
+            # First check to see if the to_add value is within the interval.
+        return main_head
+
+
+if __name__ == '__main__':
+    solutionObject = Solution()
+    l1 = createLinkedList([1, 2, 4], None)
+    l2 = createLinkedList([1, 3, 4], None)
+    solution_node = solutionObject.mergeTwoLists(l1, l2)
+    printLinkedList(solution_node)
