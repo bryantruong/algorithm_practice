fe7b7a048bcb021a8fc1bd7c23bb986f81599fa7
bryantruong <bryantruong3139@yahoo.com>
2021-07-01 10:23:48 -0400

    Implemented iterative solution

--- 21_merge_two_lists.py
+++ 21_merge_two_lists.py
@@ -62,63 +62,91 @@ def printLinkedList(head):
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        ############################################################
-        # My solution actually sucks. Somehow I way overthought it...
-        # Just google to find an actually good solution. Mine has same time complexity though.
-        ############################################################
-        # Rare Cases
-        if not l1 and not l2:
-            return None
         if not l1:
             return l2
         if not l2:
             return l1
-
-        # First determine which list will be the node that we start with (and add other list into)
-        if l1.val <= l2.val:
-            main = l1
-            to_add = l2
-        else:
-            main = l2
-            to_add = l1
-        # Save off the head of the main list to return
-        main_head = main
-        # Save off the next node in main, since we'll need it to reconnect the incoming merged section
-        main_next = main.next
-
-        # Begin iterating through the lists. Can stop once the second list (to_add list) is merged fully merged in
-        while to_add:
-            # When the main list has been iterated through, and we just need to append to_add to it
-            if main_next is None:
-                main.next = to_add
-                # Break out of the while loop
-                to_add = None
-            # Check if the current value does not fit between the interval. If not, move the main pointers.
-            elif to_add.val > main_next.val:  # We already know that it is greater than or equal to main_next
-                # Move on to the next pointers for main
-                main = main_next
-                main_next = main_next.next
+        # no point in assigning a value for the dummy head
+        dummy_head = ListNode(None)
+        # Repeat this process
+        curr_node = dummy_head
+        while l1 or l2:
+            if l1 is None:
+                curr_node.next = l2
+                return dummy_head.next
+            if l2 is None:
+                curr_node.next = l1
+                return dummy_head.next
+            if l1.val <= l2.val:
+                # In the case of a tie, l1 gets priority
+                curr_node.next = l1
+                # Advance l1
+                l1 = l1.next
             else:
-                # Need to save the current node, as well as the starting node of the to_add chain
-                curr_to_add = to_add
-                while curr_to_add.next:
-                    # At this point, if we are in this "else" block, we already know that to_add's value is valid.
-                    if curr_to_add.next.val <= main_next.val:
-                        # Move to the next node
-                        curr_to_add = curr_to_add.next
-                    else:
-                        break
-                # Rewire
-                main.next = to_add
-                to_add = curr_to_add.next
-                curr_to_add.next = main_next
+                # In the case that l2 is less than l1
+                curr_node.next = l2
+                l2 = l2.next
+            curr_node = curr_node.next
+        return "I didn't think we'd get here!"
 
-                # Advance the pointer for the next iteration of the outer loop
-                main = main_next
-                main_next = main_next.next
-
-            # First check to see if the to_add value is within the interval.
-        return main_head
+    # def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    #     ############################################################
+    #     # My solution actually sucks. Somehow I way overthought it...
+    #     # Just google to find an actually good solution. Mine has same time complexity though.
+    #     ############################################################
+    #     # Rare Cases
+    #     if not l1 and not l2:
+    #         return None
+    #     if not l1:
+    #         return l2
+    #     if not l2:
+    #         return l1
+    #
+    #     # First determine which list will be the node that we start with (and add other list into)
+    #     if l1.val <= l2.val:
+    #         main = l1
+    #         to_add = l2
+    #     else:
+    #         main = l2
+    #         to_add = l1
+    #     # Save off the head of the main list to return
+    #     main_head = main
+    #     # Save off the next node in main, since we'll need it to reconnect the incoming merged section
+    #     main_next = main.next
+    #
+    #     # Begin iterating through the lists. Can stop once the second list (to_add list) is merged fully merged in
+    #     while to_add:
+    #         # When the main list has been iterated through, and we just need to append to_add to it
+    #         if main_next is None:
+    #             main.next = to_add
+    #             # Break out of the while loop
+    #             to_add = None
+    #         # Check if the current value does not fit between the interval. If not, move the main pointers.
+    #         elif to_add.val > main_next.val:  # We already know that it is greater than or equal to main_next
+    #             # Move on to the next pointers for main
+    #             main = main_next
+    #             main_next = main_next.next
+    #         else:
+    #             # Need to save the current node, as well as the starting node of the to_add chain
+    #             curr_to_add = to_add
+    #             while curr_to_add.next:
+    #                 # At this point, if we are in this "else" block, we already know that to_add's value is valid.
+    #                 if curr_to_add.next.val <= main_next.val:
+    #                     # Move to the next node
+    #                     curr_to_add = curr_to_add.next
+    #                 else:
+    #                     break
+    #             # Rewire
+    #             main.next = to_add
+    #             to_add = curr_to_add.next
+    #             curr_to_add.next = main_next
+    #
+    #             # Advance the pointer for the next iteration of the outer loop
+    #             main = main_next
+    #             main_next = main_next.next
+    #
+    #         # First check to see if the to_add value is within the interval.
+    #     return main_head
 
 
 if __name__ == '__main__':
