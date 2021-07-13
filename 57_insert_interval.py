86576291985af119f489f612d4e2188acd345a3a
Bryan Truong <Bryan.Truong@ibm.com>
2021-07-13 10:27:54 -0400

    Implemented Insert Interval. Not very efficient.

--- 57_insert_interval.py
+++ 57_insert_interval.py
@@ -1,27 +1,38 @@
 import bisect
 
+
 class Solution:
     def insert(self, intervals, newInterval):
-        new_intervals = []
-        # A more normal approach would be to just iterate through from the start and copy over,
-        # but I want to try out the bisect Python library.
-        if not intervals:
-            new_intervals.append(newInterval)
-            return new_intervals
-        # This index is to the right of the closest index
-        closest_index = bisect.bisect(intervals, newInterval)
-        new_intervals = intervals[:closest_index]
-        curr_index = closest_index - 1
-        if new_intervals[curr_index][1] >= newInterval[0]:
+        bisect.insort(intervals, newInterval)
+        # Now just call the merge routine with this unmerged list
+        return self.merge(intervals)
 
-            print("This is where we'd replace the back")
-        print("Closest Index: " + str(closest_index))
+    def merge(self, intervals_list):
+        """
+        Helper function to merge the list. Is not in place, will return a copy of the original array
+        if there were no merges that needed to take place.
+        :param intervals_list: the list of intervals that may/may not need to be merged
+        :return: the merged list
+        """
+        solution_list = [intervals_list[0]]
+        existing_interval = solution_list[0]
+        for interval in intervals_list:
+            # If the current interval needs to be merged
+            if interval[0] <= existing_interval[1]:
+                # Check if it extends the current interval
+                if interval[1] > existing_interval[1]:
+                    solution_list[-1] = [existing_interval[0], interval[1]]
+                    existing_interval = [existing_interval[0], interval[1]]
+            else:
+                solution_list.append(interval)
+                existing_interval = solution_list[-1]
+        return solution_list
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test_case = [[1,2],[3,5],[6,7],[8,10],[12,16]]
-    test_new_interval = [11,13]
+    test_case = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+    test_new_interval = [11, 13]
     solution_instance = Solution()
     print(solution_instance.insert(test_case, test_new_interval))
 
