69ae0c3a803d9bc2f6e4afaf2a768398668df2e1
Bryan Truong <Bryan.Truong@ibm.com>
2022-04-22 09:59:34 -0400

    Cleanup

--- 1065_index_pairs_of_string.py
+++ 1065_index_pairs_of_string.py
@@ -3,7 +3,7 @@ Not a good solution. Use a prefix trie, but that is out of the scope of what I w
 """
 
 class Solution:
-    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+    def indexPairs(self, text: str, words):
         solution_list = []
         for word in words:
             start = text.find(word)
