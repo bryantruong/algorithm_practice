642104b10ddfa46b8fb74fb72f1588137dcb5392
Bryan Truong <Bryan.Truong@ibm.com>
2022-04-22 09:59:06 -0400

    kind of a waste of time

---
+++ 1065_index_pairs_of_string.py
@@ -0,0 +1,18 @@
+"""
+Not a good solution. Use a prefix trie, but that is out of the scope of what I want to learn at the moment.
+"""
+
+class Solution:
+    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+        solution_list = []
+        for word in words:
+            start = text.find(word)
+            if start != -1:
+                solution_list.append((start, start + len(word) - 1))
+                # We have to continue to search the rest of the string for the same word
+                while start != -1:
+                    start = text.find(word, start + 1)
+                    if start != -1:
+                        solution_list.append((start, start + len(word) - 1))
+        solution_list.sort()
+        return solution_list
