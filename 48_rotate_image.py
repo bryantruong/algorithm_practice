c32125293ea243a2475fecd95948d8eb7843b4e3
Bryan Truong <Bryan.Truong@ibm.com>
2022-04-26 15:19:37 -0400

    annoying array questions continued

--- 48_rotate_image.py
+++ 48_rotate_image.py
@@ -1,5 +1,5 @@
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
