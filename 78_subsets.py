91d88bc0b04e4b14e1be0a8e902dfc84b17279c3
Bryan Truong <Bryan.Truong@ibm.com>
2022-05-01 13:56:53 -0400

    looking at NeetCode's solution

--- 78_subsets.py
+++ 78_subsets.py
@@ -1,25 +1,25 @@
 class Solution:
-    def subsets(self, nums):
-        solution = [()]  # I am hardcoding the empty list, since this will always be included
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
 
-        def generate_subsets(current_list):
-            # The empty list case is already handled outside of this recursive f(n)
-            if len(current_list) == 0:
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
                 return
-            # This algorithm will remove an element from each of the possibe indices
-            for index_to_remove in range(len(current_list)):
-                # Hard copying, since we want to restore the index we remove for later iters
-                hard_copied_list = current_list.copy()
-                hard_copied_list.pop(index_to_remove)
-                generate_subsets(hard_copied_list)
-            solution.append(tuple(current_list))
-            return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
 
-        generate_subsets(nums)
-        return set(solution)
+        dfs(0)
+        return res
 
 
 if __name__ == '__main__':
-    test_case = [1, 2, 3, 4]
+    test_case = [1, 2, 3]
     solution_instance = Solution()
     print(solution_instance.subsets(test_case))
